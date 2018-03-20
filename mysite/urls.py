dae7aa53dae5e11372c05e919959a38043c8a918
Mona Strenaer <mona.strenaer@uclouvain.be>
2018-03-20 15:20:06 +0100

    Log out user

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
